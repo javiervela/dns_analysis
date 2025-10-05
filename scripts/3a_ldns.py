806eebfe7bbe6037ca615f3639858bfb4e0509d3
Javier Vela <javier.vela00@gmail.com>
2025-10-05 18:27:47 -0400

    Add script to check LDNS that respond to norecurse and RTT times

--- scripts/3a_ldns.py
+++ scripts/3a_ldns.py
@@ -1,9 +1,10 @@
 import pandas as pd
+from time import sleep
 
 from dnsclient import DNSClient
 
 INPUT_FILE = "data/ldns.csv"
-OUTPUT_FILE = "data/ldns.csv"
+TEX_FILE = "data/3_ldns_rtt.tex"
 
 df = pd.read_csv(INPUT_FILE)
 
@@ -14,11 +15,35 @@ def remove_trailing_dot(records):
     return [r[:-1] if r.endswith(".") else r for r in records]
 
 
-test_hostname = "wpi.edu"
+test_hostname = "google.com"
 
 df["Hostname"] = df["IP"].apply(
-    lambda ip: (remove_trailing_dot(client.reverse_lookup(ip)) or [None])[0]
+    lambda ip: (remove_trailing_dot(client.reverse_lookup(ip_address=ip)) or [None])[0]
 )
-df["Works"] = df["IP"].apply(lambda ip: client.get_a_record(test_hostname, ip) != [])
 
-df.to_csv(OUTPUT_FILE, index=False)
+df["A Record"] = df["IP"].apply(
+    lambda ip: (
+        client.get_a_record(domain=test_hostname, dns_server=ip, norecurse=True)
+        or [None]
+    )[0]
+)
+df["Works"] = df["A Record"].apply(lambda record: record is not None and record != "")
+df["RTT"] = df["IP"].apply(
+    lambda ip: client.get_query_time(
+        domain=test_hostname, dns_server=ip, norecurse=True
+    )
+)
+
+df.to_csv(INPUT_FILE, index=False)
+
+df = df.sort_values(by="RTT")
+df.to_latex(
+    TEX_FILE,
+    index=False,
+    columns=["IP", "Hostname", "RTT"],
+    header=["LDNS IP", "Hostname", "RTT (ms)"],
+    na_rep="N/A",
+    longtable=True,
+    caption="LDNS Response Times",
+    label="tab:ldns_rtt",
+)
