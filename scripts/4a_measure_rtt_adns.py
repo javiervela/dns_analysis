a1ea9e32e9c1e9baac4352bde7802b2ba262a10f
Javier Vela <javier.vela00@gmail.com>
2025-10-08 14:18:58 -0400

    Refactor RTT measurement logic to include separate ADNS and LDNS RTT values

--- scripts/4a_measure_rtt_adns.py
+++ scripts/4a_measure_rtt_adns.py
@@ -16,14 +16,19 @@ client = DNSClient()
 n_measurements = 10
 test_domain_fmt = "hopefullythisdoesnotexist.{}"
 
-data = [] 
+data = []
 for l in ldns:
     for h in hostnames:
         for _ in range(n_measurements):
             test_domain = test_domain_fmt.format(h)
-            rtt = client.get_query_time(domain=test_domain, dns_server=l)
-            data.append({"LDNS": l, "Hostname": h, "RTT": rtt})
-            print(f"LDNS: {l}, Hostname: {h}, RTT: {rtt} ms")
+            rtt_adns = client.get_query_time(domain=test_domain, dns_server=l)
+            rtt_ldns = client.get_query_time(
+                domain=test_domain, dns_server=l, norecurse=True
+            )
+            data.append(
+                {"LDNS": l, "Hostname": h, "ADNS_RTT": rtt_adns, "LDNS_RTT": rtt_ldns}
+            )
+            print(f"Measured {l} {h} {rtt_adns} {rtt_ldns}")
 
 df = pd.DataFrame(data)
 df.to_csv(OUTPUT_FILE, index=False)
